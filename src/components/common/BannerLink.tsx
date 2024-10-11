e65cca3ac330bf5cfea2831aeb17338f39245c6d
starcradle101 <dreamerdev12@gmail.com>
2024-10-11 11:31:12 +0900

    design: tailwindcss 커스텀 컬러 설정

--- src/components/common/BannerLink.tsx
+++ src/components/common/BannerLink.tsx
@@ -5,7 +5,7 @@ export default function BannerLink({ url }: { url: string }) {
     <Link
       href={url}
       className="
-        text-[#F5F1DE] rounded-[5px] hover:bg-opacity-90 bg-[#4A734E] text-center flex flex-row gap-2.5 items-center justify-center 
+        text-bannerLinkText rounded-[5px] hover:bg-opacity-90 bg-bannerLink text-center flex flex-row gap-2.5 items-center justify-center 
         md:w-[140px] md:h-[42px] md:text-base
         w-[100px] h-8 text-sm
         lg:w-40 lg:h-[52px] lg:text-xl
