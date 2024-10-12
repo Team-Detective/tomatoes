f1046f00c9df4156cc779cb869d5002d1715e7ab
kitaePark <bkt4044@naver.com>
2024-10-12 18:26:09 +0900

    feat: 서치바 & 토마토 img 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import BannerLink from '@/components/common/BannerLink';
+import SearchBar from '@/components/common/SearchBar';
 
 export default function Home() {
   return (
     <>
       <h1>main component</h1>
       <div className="flex flex-col gap-4 justify-center h-screen items-center">
+        {/* <BannerLink url="https://google.com" />
         <BannerLink url="https://google.com" />
-        <BannerLink url="https://google.com" />
-        <BannerLink url="https://google.com" />
+        <BannerLink url="https://google.com" /> */}
+        <SearchBar />
       </div>
     </>
   );
