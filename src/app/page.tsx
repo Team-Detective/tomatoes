a02656d90114c9103ac83a41d0c70d6297ee19fc
StopSoo <jpy1030@naver.com>
2024-10-14 00:20:25 +0900

    fix: tailwind 폰트 적용 문제 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import BannerLink from '@/components/common/BannerLink';
 import Dday from '@/components/common/Dday';
 import Tag from '@/components/common/Tag';
 
@@ -6,11 +5,6 @@ export default function Home() {
   return (
     <>
       <h1>main component</h1>
-      <div className="flex flex-col gap-4 justify-center h-screen items-center">
-        <BannerLink url="https://google.com" />
-        <BannerLink url="https://google.com" />
-        <BannerLink url="https://google.com" />
-      </div>
       <div className="flex gap-2 mb-4">
         <Tag type="hot" label="HOT" />
         <Tag type="best" label="BEST" />
