9168abab3b392198ed9a91272bf66a95bf015292
kitaePark <bkt4044@naver.com>
2024-10-14 10:12:18 +0900

    chore: page.tsx dev 브랜치로 현상 유지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,7 @@ export default function Home() {
   return (
     <>
       <h1>main component</h1>
-      <div className="flex flex-col gap-4 justify-center h-screen items-center">
-        {/* <BannerLink url="https://google.com" />
-        <BannerLink url="https://google.com" />
-        <BannerLink url="https://google.com" /> */}
-        <SearchBar placeholder="나는야 춤을 출거야🎵" />
-        <SearchBar placeholder="사과가 되지말고 토마토가 돼라" />
-      </div>
+      <div className="flex flex-col gap-4 justify-center h-screen items-center"></div>
     </>
   );
 }
